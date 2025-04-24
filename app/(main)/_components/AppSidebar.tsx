6ac8a5991efc92d575d3abf89c4cb44406c8124c
NitroRonak <ronaks0988@gmail.com>
2025-04-24 13:19:04 +0530

    Sidebar Trigger UI Changed.

--- app/(main)/_components/AppSidebar.tsx
+++ app/(main)/_components/AppSidebar.tsx
@@ -16,26 +16,30 @@ import { SideBarOptions } from "@/services/Constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 export function AppSidebar() {
-    const path=usePathname()
+  const path = usePathname();
   return (
     <Sidebar>
       <SidebarHeader className="flex items-center">
         <Image src={Logo} alt="Recruitron" className="w-[200px] h-[150px]" />
-        <Button className="w-full cursor-pointer">
-          <BsPlus /> Create New Interview
-        </Button>
+        <Link href="/dashboard/create-interview" className="w-full">
+          <Button className="w-full cursor-pointer">
+            <BsPlus /> Create New Interview
+          </Button>
+        </Link>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarContent>
             <SidebarMenu className="flex flex-col justify-center">
               {SideBarOptions.map((option) => (
-                <SidebarMenuItem
-                className="p-1"
-                  key={option.name}
-                >
-                  <Link className={`flex items-center p-4 gap-3 hover:bg-muted ${path===option.path&&"bg-blue-600"} rounded-md`} href={option.path}>
-                    <option.icon  />
+                <SidebarMenuItem className="p-1" key={option.name}>
+                  <Link
+                    className={`flex items-center p-4 gap-3 hover:bg-muted ${
+                      path === option.path && "bg-blue-600"
+                    } rounded-md`}
+                    href={option.path}
+                  >
+                    <option.icon />
                     {option.name}
                   </Link>
                 </SidebarMenuItem>
