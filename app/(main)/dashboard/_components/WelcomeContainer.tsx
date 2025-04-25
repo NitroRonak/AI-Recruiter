051033fed5012116c86ae65ce99bcf30fa99bd49
NitroRonak <ronaks0988@gmail.com>
2025-04-25 13:36:18 +0530

    Logout Button Added

--- app/(main)/dashboard/_components/WelcomeContainer.tsx
+++ app/(main)/dashboard/_components/WelcomeContainer.tsx
@@ -1,8 +1,22 @@
 "use client";
 import { userDetails } from "@/app/provider";
+import { Button } from "@/components/ui/button";
+import { supabase } from "@/services/supabaseClient";
+import { LogOutIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 const WelcomeContainer = () => {
   const { user } = userDetails();
+  const router = useRouter();
+
+  const handleLogout = async() => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Logout failed:", error.message);
+    } else {
+      router.push("/auth"); // redirect to landing/login page
+    }
+  }
   return (
     <div>
       <div className=" bg-[rgb(16,23,39)] shadow-md rounded-md px-10 py-3 flex items-center justify-between">
@@ -12,10 +26,19 @@ const WelcomeContainer = () => {
             AI-Driven Interviews , Hiring Made Easy
           </h2>
         </div>
-        <div>
+        <div className="flex items-center gap-2">
           {user && (
-            <Image src={user?.picture} alt="user" width={50} height={50} className="rounded-full" />
+            <Image
+              src={user?.picture}
+              alt="user"
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
           )}
+          <Button className="bg-transparent text-white cursor-pointer hover:bg-transparent hover:text-blue-600 " onClick={handleLogout}>
+            Logout <LogOutIcon size={30}/>
+          </Button>
         </div>
       </div>
     </div>
