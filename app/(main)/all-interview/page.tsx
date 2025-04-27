e91149a3e2803a582fe6af8110f646dd13c7984a
NitroRonak <ronaks0988@gmail.com>
2025-04-28 02:19:28 +0530

    ts-ignore

--- app/(main)/all-interview/page.tsx
+++ app/(main)/all-interview/page.tsx
@@ -21,6 +21,7 @@ const AllInterview = () => {
       .select("*")
       .eq("userEmail", user?.email)
       .order("created_at", { ascending: false });
+      // @ts-ignore
     setInterviewsList(Interview);
   };
 
